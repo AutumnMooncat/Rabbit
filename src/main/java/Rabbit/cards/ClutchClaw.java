e9a7c96ae70fe5fb4cfa2ecfe56d78f8cb725e3f
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-27 10:13:35 -0300

    ClutchClaw rework. Cost 1 -> 0. Damage 9(12) -> 5(7). Scaling 6(8) -> 5(7).

--- src/main/java/Rabbit/cards/ClutchClaw.java
+++ src/main/java/Rabbit/cards/ClutchClaw.java
@@ -18,9 +18,9 @@ public class ClutchClaw extends AbstractClutchCard {
     public final static String ID = makeID(ClutchClaw.class.getSimpleName());
 
     public ClutchClaw() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 9;
-        baseMagicNumber = magicNumber = 6;
+        super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 5;
+        baseMagicNumber = magicNumber = 5;
     }
 
     @Override
@@ -33,7 +33,7 @@ public class ClutchClaw extends AbstractClutchCard {
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(2);
         upgradeMagicNumber(2);
     }
 
