dce075d2a93f0e9c0be3be06cf91dd686c00dfa1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-27 13:10:25 -0300

    Fix NextTurnJumpPower

--- src/main/java/Rabbit/powers/NextTurnJumpPower.java
+++ src/main/java/Rabbit/powers/NextTurnJumpPower.java
@@ -2,14 +2,13 @@ package Rabbit.powers;
 
 import Rabbit.MainModfile;
 import Rabbit.actions.JumpAction;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class NextTurnJumpPower extends AbstractPower implements NonStackablePower {
+public class NextTurnJumpPower extends AbstractPower {
     public static final String POWER_ID = MainModfile.makeID(NextTurnJumpPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
