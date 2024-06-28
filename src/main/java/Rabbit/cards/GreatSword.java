9eea51e139eb92e08357c7cc4b3a2ff24f9a1590
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-28 11:54:07 -0300

    GreatSword rework. Damage lowered 14(18) -> 10(14). Block lowered 7(9) -> 5(7). Now hits all enemies.

--- src/main/java/Rabbit/cards/GreatSword.java
+++ src/main/java/Rabbit/cards/GreatSword.java
@@ -3,11 +3,15 @@ package Rabbit.cards;
 import Rabbit.cards.abstracts.AbstractClutchCard;
 import Rabbit.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.red.HeavyBlade;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
 
 import static Rabbit.MainModfile.makeID;
 
@@ -15,16 +19,19 @@ public class GreatSword extends AbstractClutchCard {
     public final static String ID = makeID(GreatSword.class.getSimpleName());
 
     public GreatSword() {
-        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseDamage = damage = 14;
-        baseBlock = block = 7;
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        baseDamage = damage = 10;
+        baseBlock = block = 5;
         baseMagicNumber = magicNumber = 2;
+        isMultiDamage = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
-        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        addToBot(new SFXAction("ATTACK_HEAVY"));
+        addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
+        addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new VulnerablePower(mon, magicNumber, false)));
     }
 
     @Override
