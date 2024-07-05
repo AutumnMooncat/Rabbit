3b6dbade7669f57013987ced7d6059aaf5768915
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-07-05 10:53:52 -0300

    Clean imports

--- src/main/java/Rabbit/cards/Plating.java
+++ src/main/java/Rabbit/cards/Plating.java
@@ -1,12 +1,8 @@
 package Rabbit.cards;
 
-import Rabbit.actions.DoAction;
 import Rabbit.actions.MultiUpgradeAction;
 import Rabbit.cards.abstracts.AbstractClutchCard;
 import Rabbit.util.ArchetypeHelper;
-import Rabbit.util.Wiz;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.colorless.RitualDagger;
 import com.megacrit.cardcrawl.cards.red.Armaments;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
